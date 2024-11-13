098beb6353176d3e32f10449d7b5df23613fdcab
daisypjm <daisy.morrison@live.co.uk>
2024-11-13 12:54:32 +0000

    Added test for complete/incomplete items, tests for edit/delete not yet working

--- src/test/java/NavigateToTodo.java
+++ src/test/java/NavigateToTodo.java
@@ -33,31 +33,82 @@ public class NavigateToTodo {
         inputPage.navigate();
 
         // REMEMBER: Come back and add input using data driven testing
-        // TEST: generic input should add to list
+        // Test Case 1: Adding different values - generic input should add to list
         inputPage.inputItem("hello123");
         assertEquals("hello123", inputPage.getFirstItem());
         inputPage.deleteList();
 
-        // TEST: blank space as input should add to list - confirm with Myrto, technically should fail as we don't want blank spaces.
+        // Test Case 1: Adding different values - blank space as input should add to list - confirm with Myrto, technically should fail as we don't want blank spaces.
         inputPage.inputItem(" ");
         assertEquals(" ", driver.findElement(By.id("todo-input")).getAttribute("value"));
         inputPage.navigate();
 
-        // TEST: no input as test should not add to list - will add later & speak to Neil
+        // Test Case 1: Adding different values - no input as test should not add to list - will add later & speak to Neil
         //driver.findElement(By.id("todo-input")).click().sendKeys(Keys.ENTER);
         //assertEquals("What needs to be done?", driver.findElement(By.id("todo-input")).getAttribute("value"));
 
-        // TEST: 255 characters as input should add to list
+        // Test Case 1: Adding different values - 255 characters as input should add to list
         inputPage.inputItem("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore.");
         assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore.", inputPage.getFirstItem());
         inputPage.deleteList();
 
-        // TEST: 254 characters as input should add to list
+        // Test Case 1: Adding different values - 254 characters as input should add to list
         inputPage.inputItem("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.");
         assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.", inputPage.getFirstItem());
         inputPage.deleteList();
 
     }
+
+    @Test
+    void ShouldEditDeleteValues() throws Exception {
+        TodoInputPage inputPage = new TodoInputPage(driver);
+        inputPage.navigate();
+
+        // Test Case 2: Editing an item
+        //inputPage.inputItem("hello123");
+        //assertEquals("hello123", inputPage.getFirstItem());
+        //takeScreenshot(driver, "preEdit.png");
+        //inputPage.editItem("hello456");
+        //takeScreenshot(driver, "postEdit.png");
+        //assertEquals("hello456", inputPage.getFirstItem());
+
+        // Test Case 2: Deleting an item
+        //inputPage.inputItem("hello123");
+        //assertEquals("hello123", inputPage.getFirstItem());
+        //takeScreenshot(driver, "preDelete.png");
+        //inputPage.deleteItem();
+        //takeScreenshot(driver, "postDelete.png");
+        //assertEquals("hello456", inputPage.getFirstItem());
+    }
+
+    @Test
+    void ShouldCompleteIncompleteValues() throws Exception {
+        TodoInputPage inputPage = new TodoInputPage(driver);
+        inputPage.navigate();
+
+        // Test Case 3: Marking items as complete and incomplete
+        inputPage.inputItem("hello1");
+        inputPage.inputItem("hello2");
+        inputPage.inputItem("hello3");
+        assertEquals("3 items left!", inputPage.countRemaining());
+        // single complete
+        inputPage.completeItem();
+        assertEquals("2 items left!", inputPage.countRemaining());
+        // all complete
+        inputPage.completeAll();
+        assertEquals("0 items left!", inputPage.countRemaining());
+        // all incomplete
+        inputPage.completeAll();
+        assertEquals("3 items left!", inputPage.countRemaining());
+        // all complete - gets repetitive here, perhaps we rearrange?
+        inputPage.completeAll();
+        assertEquals("0 items left!", inputPage.countRemaining());
+        // single incomplete
+        inputPage.completeItem();
+        assertEquals("1 item left!", inputPage.countRemaining());
+        inputPage.deleteList();
+    }
+
 
         // REMEMBER: These tests fail and need to be logged as bugs for the team to fix
     @Test
@@ -65,13 +116,19 @@ public class NavigateToTodo {
         TodoInputPage inputPage = new TodoInputPage(driver);
         inputPage.navigate();
 
-        // TEST: over 255 characters as input should fail, currently it passes
+        // Test Case 1: Adding different values - over 255 characters as input should fail, currently it passes
         inputPage.inputItem("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolores.");
         if (driver.findElement(By.cssSelector("li:nth-child(1)")).isDisplayed()) {
             fail("Your input is too long to add to the list");
         } inputPage.deleteList();
 
-        // TEST: symbols as input ("!@£$%^&*(){}[]:";'<>?") should add to list, currently it doesn't
+        // Test Case 1: Adding different values - over 1000 characters as input should fail, currently it passes
+        inputPage.inputItem("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolore.");
+        if (driver.findElement(By.cssSelector("li:nth-child(1)")).isDisplayed()) {
+            fail("Your input is too long to add to the list");
+        } inputPage.deleteList();
+
+        // Test Case 1: Adding different values - symbols as input ("!@£$%^&*(){}[]:";'<>?") should add to list, currently it doesn't
         inputPage.inputItem("!@£$%^&*(){}[]:\";'<>?");
         assertEquals("!@£$%^&*(){}[]:\";'<>?", inputPage.getFirstItem());
         inputPage.deleteList();
--- src/test/java/TodoInputPage.java
+++ src/test/java/TodoInputPage.java
@@ -1,12 +1,24 @@
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TodoInputPage {
     protected WebDriver driver;
     private By inputBoxBy = By.id("todo-input");
     private By searchItemsInList = By.cssSelector("li:nth-child(1)");
+    private By selectItemsInList = By.cssSelector("li:nth-child(1) .toggle");
+    private By deleteItemsInList = By.cssSelector("li:nth-child(1) .destroy");
     private By completeAllItems = By.className("toggle-all");
     private By deleteCompleted = By.className("clear-completed");
+    private By countRemaining = By.className("todo-count");
 
+
+    // Navigating around the site
     public TodoInputPage(WebDriver driver) {
         this.driver = driver;
     }
@@ -19,9 +31,26 @@ public class TodoInputPage {
         driver.get("https://todomvc.com/examples/react/dist/");
     }
 
+    // Inputting, editing, deleting items
     public void inputItem(String item) {
         WebElement inputItem = driver.findElement(inputBoxBy);
         inputItem.sendKeys(item+Keys.ENTER);
+    }
+
+    public void editItem(String newItem) {
+        WebElement editItem = driver.findElement(searchItemsInList);
+        Actions act = new Actions(driver);
+        act.doubleClick(editItem).perform();
+        editItem.sendKeys(Keys.CONTROL,"a", Keys.DELETE);
+        editItem.sendKeys(newItem+Keys.ENTER);
+    }
+
+    public void deleteItem() throws InterruptedException {
+        WebElement waitForDeletion = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(driver -> driver.findElement(deleteItemsInList));
+        WebElement deleteItem = driver.findElement(deleteItemsInList);
+        Thread.sleep(5000);
+        deleteItem.click();
     }
 
     public void deleteList() {
@@ -31,8 +60,28 @@ public class TodoInputPage {
         deleteAll.click();
     }
 
+    // Completing single item in list
+    public void completeItem() {
+        WebElement completeItem = driver.findElement(selectItemsInList);
+        completeItem.click();
+
+    }
+
+    // Completing all items in list
+    public void completeAll() {
+        WebElement completeAll = driver.findElement(completeAllItems);
+        completeAll.click();
+    }
+
+
+    // Returning items from list
     public String getFirstItem() {
         return driver.findElement(searchItemsInList).getText();
+
+    }
+
+    public String countRemaining() {
+        return driver.findElement(countRemaining).getText();
 
     }
 
